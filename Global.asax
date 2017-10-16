22e4c9223e6f864fa9fa8c143c4122d45c096e71
Viet-Hoa <32198887+Viet-Hoa@users.noreply.github.com>
2017-10-16 19:01:27 +0700

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QLsach.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLsach.Startup))]
+namespace QLsach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
